ff373894cafbe954963a9bf755662e00279ec989
Anastasia Bykasova <nastybykasova@gmail.com>
2022-11-27 18:44:10 +0300

    Co-authored-by: ZakharkinaAnastasia <ZakharkinaAnastasia@users.noreply.github.com> Co-authored-by: boyunovaaa <boyunovaaa@users.noreply.github.com>

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset = "UTF-8">
+    <link rel = 'stylesheet' href = 'style.css'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nabla&family=Open+Sans:ital,wght@0,400;0,500;1,400;1,500&family=Poppins&display=swap" rel="stylesheet">
     <title>Система оценки качества работы преподавателя</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Система оценки качества работы преподавателя</h1>
-    <h2>Данный проект выполняется студентами Москвоского Политехничесого университета.</h2>
-    <h2>Научный руководитель - Логачёв Максим Сергеевич.</h2>
-
-
-</body>
+    <div class="header">
+        <div class="container">
+            <div class="header-line">
+                <h1 class="h">Домашняя страница</h1>
+                <a class="nav-item" href="#body">Главная</a>
+                <a class="nav-item" href="#contact">Контакты</a>
+            </div>
+        </div>    
+    </div>
+    <div class="header-down">
+        <h1>Система оценки качества работы преподавателя</h1>
+        <h2>Данный проект выполняется студентами Москвоского Политехничесого университета.</h2>
+        <h2>Научный руководитель - Логачёв Максим Сергеевич.</h2>
+    </div>
+    <div class="footer">
+        <div class="header-line">
+            <p class="foot" id = "email">Почта: </p>
+            <p class="foot" id = "contact">Телефон: </p>
+            <!--<a class="nav" href="communication.php">Обратная связь</a>-->
+        </div>   
+    </div>
+</body>  
 </html>
