448e01db80d45e8d70477fcac4096d74e256d319
arahisss <maindelay@mail.ru>
2022-11-27 15:52:53 +0300

    Start of project

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Система оценки качества работы преподавателя</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Система оценки качества работы преподавателя</h1>
+    <h2>Данный проект выполняется студентами Москвоского Политехничесого университета.</h2>
+    <h2>Научный руководитель - Логачёв Максим Сергеевич.</h2>
+
+
+</body>
+</html>
