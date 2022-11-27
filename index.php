ab7cfea61997ba1bc8c1d27e7a864c3639a3ae55
arahisss <maindelay@mail.ru>
2022-11-27 20:45:40 +0300

    Пытаюсь запустить запрос на сервере

--- index.php
+++ index.php
@@ -8,9 +8,40 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Система оценки качества работы преподавателя</h1>
-    <h2>Данный проект выполняется студентами Москвоского Политехничесого университета.</h2>
+    <h1>Проект "Система оценки качества работы преподавателя"</h1>
+    <h2>Данный проект выполняется студентами Московского Политехнического университета.</h2>
     <h2>Научный руководитель - Логачёв Максим Сергеевич.</h2>
+
+    <?php 
+        // http://api.elsevier.com/content/search/author?query=AFFIL%28university%29
+        // https://api.elsevier.com/content/search/author?query=authlast(Einstein)%20and%20authfirst(Albert)%20and%20affil(Princeton)&apiKey=7f59af901d2d86f78a1fd60c1bf9426a
+
+        $url = 'https://api.elsevier.com/content/search/author';
+
+        $options = array(
+            'apiKey' => 'c718020d82709e407c135fb4a33a9a06',
+            'authlast' => 'Einstein',
+            'authfirst' => 'Albert',
+            'affil' => 'Princeton'
+            
+        );
+        //'lang' => 'en'
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url.'?'.http_build_query($options));
+
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        echo '<pre>';
+        print_r($response);
+
+        
+    ?>
+
+    <script type="text/javascript" src="https://api.elsevier.com/content/search/author"></script>
+    <script type="text/javascript" src="main.js"></script>
 
 
 </body>
---
+++ main.js
@@ -0,0 +1,8 @@
+var key = 'c718020d82709e407c135fb4a33a9a06';
+
+searchForm.addEventListener('submit', submitSearch);
+
+// https://api.elsevier.com/content/search/author?query=authlast(Einstein)%20and%20authfirst(Albert)%20and%20affil(Princeton)&apiKey=c718020d82709e407c135fb4a33a9a06
+
+
+// https://api.elsevier.com/content/abstract/citation-count?doi=10.1016/S0014-5793(01)03313-0&httpAccept=text/html&apiKey=c718020d82709e407c135fb4a33a9a06
